76a1ab1b29aa64b10de59c2906d62b1bc6ed9ec0
Luis Alberto Quesada Martinez <luisquesada163@gmail.com>
2022-07-25 15:24:33 -0300

    condicionais
    if

--- aula11/ex008.js
+++ aula11/ex008.js
@@ -1,1 +1,8 @@
-console.log('O console funcionou correctamente');
+var vel = 78.5;
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+if (vel > 60) {  //Condição simple!
+    console.log('Você ultrapassou a velocidade permitada')
+}
+console.log('Dirija sempre usando cinto de segurança')
+
+
