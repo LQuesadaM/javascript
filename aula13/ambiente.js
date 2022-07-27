7fec080fa3bb686b2ebbc1f70b15dffd086e61fd
Luis Alberto Quesada Martinez <luisquesada163@gmail.com>
2022-07-27 17:27:52 -0300

    Bucles
    while
    do While

--- aula13/ambiente.js
+++ aula13/ambiente.js
@@ -1,5 +1,18 @@
-var contador = 1
-while (contador < 6) {
-    console.log('Tudo bem')
-    c++ // c = c + 1
+
+var contador1 = 1
+var contador2 = 1
+
+// La estructura while es clasificada como estrutura de repeticion con teste logico de inicio. Si el teste logico es true continua el loop, si es false para
+
+while (contador1 <= 6) {
+    console.log(`Passo ${contador1}`)
+    contador1++ // c = c + 1
 }
+
+
+
+// Ejecuta el bloque y despues hace el teste. Estructura de repeticion con teste logico no final
+do {
+    console.log(`cont ${contador2}`)
+    contador2++
+} while (contador2 <= 6)
