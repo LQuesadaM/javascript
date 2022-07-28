455cec30f26e5d026d5fc5e03e8ec79b49983668
Luis Alberto Quesada Martinez <luisquesada163@gmail.com>
2022-07-28 10:47:56 -0300

    Update while-.do while.js

--- aula13/while-.do while.js
+++ aula13/while-.do while.js
@@ -1,9 +1,9 @@
 
+
+
+// La estructura while es clasificada como estrutura de repeticion con teste logico de inicio. Si el teste logico es true continua el loop, si es false para.
+
 var contador1 = 1
-var contador2 = 1
-
-// La estructura while es clasificada como estrutura de repeticion con teste logico de inicio. Si el teste logico es true continua el loop, si es false para
-
 while (contador1 <= 6) {
     console.log(`Passo ${contador1}`)
     contador1++ // c = c + 1
@@ -12,7 +12,10 @@ while (contador1 <= 6) {
 
 
 // Ejecuta el bloque y despues hace el teste. Estructura de repeticion con teste logico no final
+
+var contador2 = 1
 do {
     console.log(`cont ${contador2}`)
     contador2++
 } while (contador2 <= 6)
+
