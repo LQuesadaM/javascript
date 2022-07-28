51dbfa85dc95a36d6db473167bc519f5cc94954f
Luis Alberto Quesada Martinez <luisquesada163@gmail.com>
2022-07-28 10:41:01 -0300

    Estructura de repeticion con variable de Control
    Estructura for

---
+++ aula14/for.js
@@ -0,0 +1,17 @@
+/*
+var contador1 = 1
+
+while(contador1 <= 6) {
+    console.log(contador1)
+    contador1++
+}
+*/
+//-----------------------------------------
+
+// Estructura de Repeticion con variables de Control
+
+console.log('Vai començar...')
+for (var cont = 1; cont <= 4;cont++ ) {
+    console.log(cont)
+}
+console.log('FIM!')
